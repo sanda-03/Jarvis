4198e62723bf6238fcfc7bade9b0d31884ed52d0
sanda-03 <165825386+sanda-03@users.noreply.github.com>
2025-07-24 23:11:42 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from google import genai
 client = genai.Client()
-key="AIzaSyA5e7iOo71Kj-rEX__-RG4ZVHjxl8weMRw"
+# key= <get your own key>
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
 )
